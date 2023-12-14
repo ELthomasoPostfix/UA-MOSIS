4224c9636f653efb6c0bdf329e885c5d8be286b9
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-12-14 22:05:05 +0100

    Add fairly complicated test.
    The assignment required a unit test
     to be added. The added test is a
     fairly complicated test for R8, R9
     which were not yet tested by the
     existing tests.

--- assignment-5-statecharts/statechart/Mosis23StartingPoint.zip_expanded/StartingPoint/trafficlight_test.py
+++ assignment-5-statecharts/statechart/Mosis23StartingPoint.zip_expanded/StartingPoint/trafficlight_test.py
@@ -79,9 +79,78 @@ SCENARIOS = [
             (3000000000, "set_yellow", True),
             (3500000000, "set_yellow", False),
         ],
-    }
+    },
 
     # You MUST add one extra scenario HERE!
+    {
+        # IMPORTANT: checks R8, R9 --> more details in further comments
+        "name": "reactive police interrupt",
+        "input_trace": [
+            # Police interrupt works for > 2s
+            (0, "button_pressed", None),
+            (3000000000, "button_released", None),
+            # R8, Switch to reactive/smart mode during interrupt
+            (3500000000, "button_pressed", None),
+            (3750000000, "button_released", None),
+            # R8, Detecting cars during police interrupt does nothing
+            (3800000000, "car_detected", None),
+            (3900000000, "car_detected", None),
+            # Second police interrupt resumes normal traffic light behavior.
+            (4000000000, "button_pressed", None),
+            (6000000000, "button_released", None),
+            # R8-extension, show properly switched to smart mode,
+            # a car detection does influence green
+            (8500000000, "car_detected", None),
+            # R8-extension Mode can be turned to dumb again normally
+            (10500000000, "button_pressed", None),
+            (11000000000, "button_released", None),
+            # R8-extension, show properly switched to dumb mode,
+            # the car detections do not influence green
+            (14500000000, "car_detected", False),
+        ],
+        "output_trace": [
+            # R9, Let normal traffic light behavior go to green before interrupt.
+            # This will show that toggling off the police interrupt resumes normal
+            # behavior at red.
+            # ==> Note: ALL traffic light colour chages are due to entry/exit actions
+            (0, "set_red", True),
+            (2000000000, "set_red", False),
+            (2000000000, "set_green", True),
+            (3000000000, "set_green", False),
+            # Yellow light ends on ON when the second interrupt event fires.
+            # ==> Exiting police interrupt turns the yellow light OFF on exit!
+            # AND police interrupt is interspersed with LED events
+            (3000000000, "set_yellow", True),
+            (3500000000, "set_yellow", False),
+            (3750000000, "set_led", True),      # R8, LED can be toggled mid-interrupt ==> switch to smart!
+            (4000000000, "set_yellow", True),
+            (4500000000, "set_yellow", False),
+            (5000000000, "set_yellow", True),
+            (5500000000, "set_yellow", False),
+            (6000000000, "set_yellow", True),
+            (6000000000, "set_yellow", False),   # OFF due to PoliceInterruptBehavior exit action
+            # R9, Return to normal behavior, enters red for safety reasons
+            (6000000000, "set_red", True),
+            # R8-extension, Run until green entered, to show smart mode still on
+            # despite interrupt being turned off.
+            # ==> a single car detection extends the total green ON time to 2.5s
+            (8000000000, "set_red", False),
+            (8000000000, "set_green", True),
+            (10500000000, "set_green", False),  # 2.5s since green ON
+            (10500000000, "set_yellow", True),
+            # R8-extension, Mode can be toggled from smart to dumb normally
+            # ==> turning the interrupt off did not break the dumb/smart mode behavior
+            (11000000000, "set_led", False),
+            # R8-extension, show properly switched to dumb mode
+            # ==> the 'car_detected' events do not influence total green time
+            (11500000000, "set_yellow", False),
+            (11500000000, "set_red", True),
+            (13500000000, "set_red", False),
+            (13500000000, "set_green", True),
+            (15500000000, "set_green", False),  # 2s since green ON, despite 'car_detected' event at 14500000000
+            (15500000000, "set_yellow", True),
+        ],
+    }
 ]
 
 # The following events are safe to repeat: (with same value)
