13fa61eb6fa6a68774b00093ee07e628fafe6350
elthomaso <GueutalThomas@outlook.com>
2023-12-27 21:49:57 +0100

    Implement basic Collector component.
    The Collector component collects Car events that
     exit the simulation.
    
    Not sure which statistics are required for this assignment,
     so they are still missing from this basic implementation.

--- assignment-6-devs/components/collector.py
+++ assignment-6-devs/components/collector.py
@@ -1,5 +1,16 @@
+from dataclasses import dataclass
+
 from pypdevs.DEVS import AtomicDEVS
 
+from components.messages import Car
+
+
+@dataclass
+class CollectorState:
+    simulated_time: float = 0.0
+    """Pattern 2: simulated time. It represents a timer that started when the simulation began. Note that this value is only updated in extTransition(), so the simulated time is accurate up until the moment where the last external event was received."""
+    n: int = 0
+    """The amount of Cars that have exited the simulation/system."""
 
 
 class Collector(AtomicDEVS):
@@ -9,3 +20,24 @@ class Collector(AtomicDEVS):
         :param block_name: The name for this model. Must be unique inside a Coupled DEVS.
         """
         super(Collector, self).__init__(block_name)
+
+        self.state: CollectorState = CollectorState()
+
+        # ports
+        self.car_in = self.addInPort("car_in")
+        """Port that receives the Cars to collect. It can be useful to already compute some statistics upon the arrival of new Cars."""
+
+    def extTransition(self, inputs):
+        """May edit state."""
+        # Update simulation time
+        self.state.simulated_time += self.elapsed
+        # Collect Car statistics
+        if self.car_in in inputs:
+            car: Car = inputs[self.car_in]
+            self.state.n += 1 
+        return self.state
+
+    # Don't define anything else, as we only store events/statistics.
+    # Collector has no behaviour of its own, so the internal transition
+    # function does not need to increase the simulated time using the
+    # time advance, as pattern 2 normally does.
