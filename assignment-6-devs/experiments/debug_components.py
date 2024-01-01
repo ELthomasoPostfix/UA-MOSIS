5f929b5762aa89d5f6839d621a0781d0da18d1b5
elthomaso <GueutalThomas@outlook.com>
2024-01-01 14:50:00 +0100

    Update debug experiment for PinPongMulti testing and updated GasStation testing.

--- assignment-6-devs/experiments/debug_components.py
+++ assignment-6-devs/experiments/debug_components.py
@@ -12,6 +12,7 @@ test_names = [
     SM, CL, GN, GS
 ]
 
+test_name = 'test'
 test_name = test_names[3]
 
 
@@ -85,6 +86,7 @@ match test_name:
         cars = [x[1] for x in cl.state["data"]]
 
     case 'gs':
+        from other.ping_pong_multi import PingPongMulti
         from tests.helpers import *
         from components.gasstation import GasStation
 
@@ -94,10 +96,10 @@ match test_name:
         pre_cars = [(x, Car(x, 15, 5, 5, v=15, departure_time=x, no_gas=True)) for x in range(n)]
         pre_sim_gas_total: int = sum([car.no_gas for _, car in pre_cars])
 
-        sc = model.addSubModel(Scheduler("sc", pre_cars))
-        pp = model.addSubModel(PingPong("pp"))
-        gs = model.addSubModel(GasStation("GASSTATION"))
-        cl = model.addSubModel(TestCollector("cl"))
+        sc = model.addSubModel(Scheduler(Scheduler.__name__, pre_cars))
+        pp = model.addSubModel(PingPongMulti(PingPongMulti.__name__, t_until_dep=5.0, pong_delay=0.2, first_x_inf=3))
+        gs = model.addSubModel(GasStation(GasStation.__name__))
+        cl = model.addSubModel(TestCollector(TestCollector.__name__))
         model.connectPorts(sc.output, gs.car_in)
         model.connectPorts(gs.Q_send, pp.inp)
         model.connectPorts(pp.out, gs.Q_rack)
@@ -114,11 +116,42 @@ match test_name:
         print("\n=== STATS ===\n")
         print("set(c.ID for c in cars) == set(x for x in range(n))   : ",set(c.ID for c in cars) == set(x for x in range(n)))
 
+        print(set(c.ID for c in cars))
+        print(set(x for x in range(n)))
+        print()
+        print()
+        print()
+
         avg_time = sum(times) / len(times)
         print("avg_time : ", avg_time)
         print("580 < avg_time < 620   : ", 580 < avg_time < 620)
         print("min(times) >= 120      : ", min(times) >= 120)
         print("sum([car.no_gas for car in cars]) (expect 0) :\n", "\tpre-sim : ", pre_sim_gas_total, "\n\tpost-sim: ", sum([car.no_gas for car in cars]))
 
+
+    case 'test':
+        from other.ping_pong_multi import PingPongMulti
+        from tests.helpers import CDEVS, Scheduler
+        from components.messages import Query
+
+        model = CDEVS("model")
+        sc = model.addSubModel(Scheduler(Scheduler.__name__, [
+            (t / 20, Query(t))
+            for t in range(6)
+            
+            
+        ] + [
+            (20, Query(20)),
+            (20, Query(21)),
+        ]))
+        ppm = model.addSubModel(PingPongMulti(PingPongMulti.__name__))
+        model.connectPorts(sc.output, ppm.inp)
+
+        sim = Simulator(model)
+        # sim.setTerminationTime(10)
+        sim.setClassicDEVS()
+        sim.setVerbose()          # Simulator tracks all events/state changes?
+
+        sim.simulate()
 
 print("\n"*4, "=== DONE ===\n")
